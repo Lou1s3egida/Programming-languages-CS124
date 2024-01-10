0192b57fe5f97946ada44b678bde663ca3135ade
Lou1s3egida <148022591+Lou1s3egida@users.noreply.github.com>
2024-01-10 09:59:53 +0800

    Add files via upload

---
+++ Codes.py
@@ -0,0 +1,29 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+print("Welcome to the Python Calculator!")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+if operation == '+':
+    print("Result:", add(num1, num2))
+elif operation == '-':
+    print("Result:", subtract(num1, num2))
+elif operation == '*':
+    print("Result:", multiply(num1, num2))
+elif operation == '/':
+   print("Result:", divide(num1, num2))
